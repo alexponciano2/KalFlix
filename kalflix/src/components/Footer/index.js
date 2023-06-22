256144d6e1606b77823be9135fa09b8ceaace113
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-06-22 20:27:54 -0300

    update footer

--- kalflix/src/components/Footer/index.js
+++ kalflix/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <h2>MaiaFlix &copy; Desenvolvido por Edson Maia 2023</h2>
+            <h2>KalFlix &copy; Desenvolvido por Alex Ponciano - 2023</h2>
         </footer>
     );
 }
