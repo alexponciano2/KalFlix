fe3d7c670a8443db5bc1cfcaf4c00134599db82b
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-07-06 10:23:49 -0300

    added Favorites component

--- kalflix/src/components/Card/index.js
+++ kalflix/src/components/Card/index.js
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
+import iconFavorites from "./favorite.png"
+import iconUnFavorites from "./unfavorite.png"
 
 function Card({ id }) {
     return (
         <section className={styles.card}>
             <Link to={`/watch/${id}`} >
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa" />
+                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+                alt="Capa" className={styles.capa}/>
             </Link>
+            <figure className={styles.icon}>
+                <img src={iconFavorites} alt="Ícone" />
+            </figure>
         </section>
     );
 }
--- kalflix/src/routes.js
+++ kalflix/src/routes.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Search from "./pages/Search";
 import Watch from "./pages/Watch";
+import Favorites from "./components/Favorites";
 
 function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ function AppRoutes() {
                 <Route path="/" element={ <Home /> } ></Route>
                 <Route path="/watch/:id" element={ <Watch /> } ></Route>
                 <Route path="/search" element={ <Search /> } ></Route>
+                <Route path="/favorites" element={ <Favorites /> } ></Route>
                 <Route path="*" element={ <PageNotFound /> } ></Route>
             </Routes>
         </BrowserRouter>
