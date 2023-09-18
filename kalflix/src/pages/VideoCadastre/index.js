abbabe2944547a08213421fddc0b942af8686aa8
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-09-18 19:55:05 -0300

    Create VideoCadastre

---
+++ kalflix/src/pages/VideoCadastre/index.js
@@ -0,0 +1,18 @@
+import Form from "../../components/Form";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+function VideoCadastre(){
+    return (
+        <>
+            <Header/>
+            <Container/>
+            <Form/>
+            <Container/>
+            <Footer/>
+        </>
+    );
+}
+
+export default VideoCadastre;
