f46bff2a64f52ce2e2ca8ee6c74d74383c115915
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-09-19 21:42:12 -0300

    Update index.js

--- kalflix/src/pages/Favorites/index.js
+++ kalflix/src/pages/Favorites/index.js
@@ -1,9 +1,10 @@
 import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 import Container from "../../components/Container";
-import styles from "./Favorites.module.css";
+import Footer from "../../components/Footer";
 import VideoList from "../../components/VideoList";
+import styles from "./Favorites.module.css";
 import { useFavoriteContext } from "../../context/Favorites";
+import ScrollToTopButton from "../../components/ScrollToTopButton";
 
 function Favorites() {
 
@@ -11,11 +12,12 @@ function Favorites() {
 
     return (
         <>
+            <ScrollToTopButton />
             <Header />
             <Container>
                 <section className={styles.favorites}>
                     <h2>Meus Favoritos</h2>
-                    { <VideoList videos={favorite}/> }
+                    { <VideoList videos={favorite} emptyHeading="🤔 Sem favoritos 🤔" /> }
                 </section>
             </Container>
             <Footer />
