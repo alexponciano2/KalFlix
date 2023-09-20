c319b0009efd4f9821eafe28ddd93456914876ff
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-09-19 21:40:36 -0300

    Update index.js

--- kalflix/src/components/Carousel/index.js
+++ kalflix/src/components/Carousel/index.js
@@ -1,20 +1,45 @@
-import "./carousel.css"
-import React, { Component } from "react";
 import Slider from "react-slick";
-
+import "./carousel.css";
 
 function Carousel({ children }) {
+
     const settings = {
         dots: false,
-        infinite: false,
+        infinite: false, // true
         speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1
-    };
+        centerMode: false,
+        variableWidth: true,
+        adaptiveHeight: true,
+        // slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    }
 
     return (
         <div>
-            <Slider {...settings}>
+            <Slider {...settings} >
                 { children }
             </Slider>
         </div>
